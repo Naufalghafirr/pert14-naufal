916c22a32a30253570a3aace2207a6cdd30e4847
GoPieRRe <naufalghafir123@gmail.com>
2025-06-25 01:13:49 +0700

    new seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-            'password' => bcrypt('12345'),
+            'password' => bcrypt('123456'),
         ]);
     }
 }
