aa6967d3f9de8056c576f3700a7fba60566f8d1a
GoPieRRe <naufalghafir123@gmail.com>
2025-06-25 02:04:27 +0700

    update new proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,13 +3,21 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Middleware\TrustProxies as Middleware; // Laravel 9+
+use Illuminate\Http\Middleware\TrustProxies as Middleware; // Laravel ≥9
 
 class TrustProxies extends Middleware
 {
-    // Terima semua proxy (Railway)
+    /**
+     * Semua proxy (Railway, Cloudflare, dll).
+     *
+     * @var array|string|null
+     */
     protected $proxies = '*';
 
-     // Gunakan hanya HEADER_X_FORWARDED_PROTO
+    /**
+     * Gunakan header X-Forwarded-Proto supaya Laravel tahu request aslinya HTTPS.
+     *
+     * @var int
+     */
     protected $headers = Request::HEADER_X_FORWARDED_PROTO;
 }
